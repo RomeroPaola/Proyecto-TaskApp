914301647b74c84d98d174e9eef73b03e0a97276
PaolaRomero <paola.rom.d@gmail.com>
2018-06-22 12:08:02 -0400

    Registro de Categoria

---
+++ Proyecto-TaskApp-master/app/src/main/java/ado/edu/itla/taskapp/repositorio/db/EstructuraDb.java
@@ -0,0 +1,6 @@
+package ado.edu.itla.taskapp.repositorio.db;
+
+public class EstructuraDb {
+
+    public static final String TABLA_CATEGORIA = "CREATE TABLE categoria (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT)";
+}
